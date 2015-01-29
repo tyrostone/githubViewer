9e18834bf0e4b695edbde9206d7aac855eb0a436
lstone <laura.stone@alumni.carleton.edu>
2015-01-29 11:46:55 -0500

    Create github service

--- script.js
+++ script.js
@@ -4,18 +4,18 @@
   var app = angular.module("githubViewer", []);
 
   var MainController = function(
-    $scope, $http, $interval,
+    $scope, github, $interval,
     $log, $anchorScroll, $location) {
 
-    var onUserComplete = function(response) {
-      $scope.user = response.data;
-      $http.get($scope.user.repos_url)
-        .then(onRepos, onError);
+    var onUserComplete = function(data) {
+      $scope.user = data;
+      github.getRepos($scope.user).then(onRepos, onError);
     };
     
-    var onRepos = function(response) {
-      $scope.repos = response.data;
+    var onRepos = function(data) {
+      $scope.repos = data;
       $location.hash("userDetails");
+      $anchorScroll();
     };
   
     var onError = function(reason) {
@@ -36,8 +36,7 @@
 
     $scope.search = function(username) {
       $log.info("Searching for " + username);
-      $http.get("https://api.github.com/users/" + username)
-        .then(onUserComplete, onError);
+      github.getUser(username).then(onUserComplete, onError);
         if (countdownInterval) {
           $interval.cancel(countdownInterval);
           $scope.countdown = null;
