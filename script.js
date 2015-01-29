f7143979e3923b26affb08b1c0b75bc0e56a8093
lstone <laura.stone@alumni.carleton.edu>
2015-01-29 11:27:19 -0500

    Add gamification and UI services

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
   
   var app = angular.module("githubViewer", []);
 
-  var MainController = function($scope, $http) {
+  var MainController = function(
+    $scope, $http, $interval,
+    $log, $anchorScroll, $location) {
 
     var onUserComplete = function(response) {
       $scope.user = response.data;
@@ -13,6 +15,7 @@
     
     var onRepos = function(response) {
       $scope.repos = response.data;
+      $location.hash("userDetails");
     };
   
     var onError = function(reason) {
@@ -21,21 +24,33 @@
 
     var decrementCountdown = function() {
       $scope.countdown -=1;
-      if (scope.countdown < 1) {
+      if ($scope.countdown < 1) {
          $scope.search($scope.username);
       }
     };
+    
+    var countdownInterval = null;
+    var startCountdown = function() {
+      countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    };
 
     $scope.search = function(username) {
+      $log.info("Searching for " + username);
       $http.get("https://api.github.com/users/" + username)
         .then(onUserComplete, onError);
+        if (countdownInterval) {
+          $interval.cancel(countdownInterval);
+          $scope.countdown = null;
+        }
     };
 
     $scope.username = "angular";
     $scope.message = "Github Viewer";
     $scope.repoSortOrder = "-stargazers_count";
     $scope.countdown = 5;
+    startCountdown();
   };
   
-  app.controller("MainController", ["$scope", "$http", MainController]);
+  // No need for minification
+  app.controller("MainController",  MainController);
 }());
